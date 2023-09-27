e534b607ab6b12313d718dece550df59f15b3191
Tommy Nguyen <Tommy.nguyenoc@gmail.com>
2023-09-27 20:42:57 +0200

    changed dateformat for newCard comp

--- src/pages/NewCard.jsx
+++ src/pages/NewCard.jsx
@@ -4,16 +4,26 @@ import { useEffect, useState } from "react";
 
 
 export default function NewCards() {
+
     const { cards, setCards } = useOutletContext();
     const [cardNumber, setCardNumber] = useState('');
     const [cardName, setCardName] = useState('');
     const [expiration, setExpiration] = useState('');
     const [ccv, setCcv] = useState('');
     const [vendor, setVendor] = useState('Visa'); 
-
+    
+    const formattedExpiration = expiration.split('-').map(part => {
+        if (part.length === 2) {
+        
+          return part;
+        } else if (part.length === 4) {
+         
+          return part.slice(2);
+        }
+      }).join('/');
 
     const updateCardPreview = () => {
-        const formattedCardPreview = `\nCard Number: ${cardNumber}\nCard Name:${cardName}\nValid Thru: ${expiration}\nCCV: ${ccv}\nVendor: ${vendor}`;
+        const formattedCardPreview = `\nCard Number: ${cardNumber}\nCard Name:${cardName}\nValid Thru: ${formattedExpiration}\nCCV: ${ccv}\nVendor: ${vendor}`;
         setCardPreview(formattedCardPreview);
     };
 
@@ -23,16 +33,21 @@ export default function NewCards() {
     useEffect(() => {
         updateCardPreview();
     }, [cardNumber, cardName, expiration, ccv, vendor]);
+    
 
     const addCard = () => {
+
+        
+
         const newCard = {
             cardNumber,
             cardName,
-            expiration,
+            expiration: formattedExpiration,
             ccv,
             vendor,
         };
 
+        
         const newCards = [...cards, newCard];
         setCards(newCards);
     };
@@ -46,25 +61,34 @@ export default function NewCards() {
                 <pre>{cardPreview}</pre>
             </div>
 
-            <label htmlFor="cardNumber">Card Number</label>
-            <input type="number" id="cardNumber" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} />
-            <br />
-            <label htmlFor="cardName">Cardholder Name</label>
-            <input type="text" id="cardName" value={cardName} onChange={(e) => setCardName(e.target.value)} />
-            <br />
-            <label htmlFor="expiration">Valid Thru</label>
-            <input type="month" id="expiration" value={expiration} onChange={(e) => setExpiration(e.target.value)} />
-            <br />
-            <label htmlFor="ccv">CCV</label>
-            <input type="number" id="ccv" value={ccv} onChange={(e) => setCcv(e.target.value)} />
-            <br />
-            <label htmlFor="vendor">Vendor</label>
-            <select id="vendor" value={vendor} onChange={(e) => setVendor(e.target.value)}>
-                <option value="Visa">Visa</option>
-                <option value="Mastercard">Mastercard</option>
-                <option value="American Express">American Express</option>
-            </select>
-            <br />
+            <div className="input-container">
+                <label htmlFor="cardNumber">Card Number</label>
+                <input type="number" id="cardNumber" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} />
+            </div>
+
+            <div className="input-container">
+                <label htmlFor="cardName">Cardholder Name</label>
+                <input type="text" id="cardName" value={cardName} onChange={(e) => setCardName(e.target.value)} />
+            </div>
+
+            <div className="input-container">
+                <label htmlFor="expiration">Valid Thru</label>
+                <input type="month" id="expiration" value={expiration} onChange={(e) => setExpiration(e.target.value)} />   
+            </div>
+
+            <div className="input-container">
+                <label htmlFor="ccv">CCV</label>
+                <input type="number" id="ccv" value={ccv} onChange={(e) => setCcv(e.target.value)} />    
+            </div>
+            <div className="input-container">
+                <label htmlFor="vendor">Vendor</label>
+                <select id="vendor" value={vendor} onChange={(e) => setVendor(e.target.value)}>
+                    <option value="Visa">Visa</option>
+                    <option value="Mastercard">Mastercard</option>
+                    <option value="American Express">American Express</option>
+                </select>   
+            </div>
+
             <button onClick={addCard}>
                 Add Card
             </button>
